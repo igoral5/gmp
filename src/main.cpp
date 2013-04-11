a436a04db2b433866b6809278665fa4aacac5ab1
Igor Alimov <alimov@shturmann.com>
2013-04-11 10:36:46 +0400

    Добавлена сборочная система autotools и src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 11.04.2013
+ *      Author: igor
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+#include <exception>
+
+int
+main(int argc, char *argv[]) try 
+{
+	std::locale::global(std::locale(""));
+
+	return EXIT_SUCCESS;
+}
+catch(const std::exception& err)
+{
+	std::cerr << "An exception occurred : " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch(...) 
+{
+    std::cerr << "Unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
