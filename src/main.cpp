7a9eb2c69150337e5fda8357b51ee5c68362d470
Igor Alimov <alimov@shturmann.com>
2013-04-11 11:38:43 +0400

    Реализована структура файлов gettext

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
  *      Author: igor
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include "gettext.h"
+#define _(string) gettext(string)
 #include <iostream>
 #include <cstdlib>
 #include <locale>
@@ -14,16 +19,17 @@ int
 main(int argc, char *argv[]) try 
 {
 	std::locale::global(std::locale(""));
-
+	bindtextdomain( PACKAGE, LOCALEDIR );
+	textdomain( PACKAGE );
 	return EXIT_SUCCESS;
 }
 catch(const std::exception& err)
 {
-	std::cerr << "An exception occurred : " << err.what() << std::endl;
+	std::cerr << _("An exception occurred : ") << err.what() << std::endl;
 	return EXIT_FAILURE;
 }
 catch(...) 
 {
-    std::cerr << "Unknown exception" << std::endl;
+    std::cerr << _("Unknown exception") << std::endl;
 	return EXIT_FAILURE;
 }
